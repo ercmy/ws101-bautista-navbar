95705879888ad4a6ce63bffbdd4ad122ffd6c9d7
ercmy <ericamaybautista08@gmail.com>
2024-09-27 19:57:00 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+import {Link} from "react-router-dom";
+
+import "./Navbar.css";
+
+export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    return (
+    <nav>
+        <Link to="/" className="home">
+            Home
+        </Link>
+        <div className='menubutton' 
+        onClick={() => {
+            setMenuOpen(!menuOpen);
+        }}
+        >
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+    </nav>
+    );
+};
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const About = () => {
+    return (
+    <div className="section-container">About</div>
+    );
+};
