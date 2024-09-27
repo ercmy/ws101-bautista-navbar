32e4b5044034cba7ba6cb619013d17459d91dcc7
ercmy <ericamaybautista08@gmail.com>
2024-09-27 21:02:29 +0800

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,33 @@
-import React, {useState} from 'react';
-
-import {Link} from "react-router-dom";
-
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-    <nav>
-        <Link to="/" className="home">
-            Home
-        </Link>
-        <div className='menubutton' 
-        onClick={() => {
-            setMenuOpen(!menuOpen);
-        }}
-        >
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <ul className={menuOpen ? "open" : ""}>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li>
-        </ul>
-    </nav>
+        <nav>
+            <div
+                className='menubutton'
+                onClick={() => {
+                    setMenuOpen(!menuOpen);
+                }}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ""}>
+                <li>
+                    <Link to="/">Home</Link> 
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
     );
 };
